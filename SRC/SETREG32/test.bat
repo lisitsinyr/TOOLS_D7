3dfc639adffe60772462d59f7a4414580522c0d0
lisitsinyr <lisitsinyr@gmail.com>
2024-06-05 10:30:55 +0400

    20240605103050

---
+++ SRC/SETREG32/test.bat
@@ -0,0 +1,9 @@
+setreg32.exe hkcu test bt b true
+setreg32.exe hkcu test bf b false
+setreg32.exe hkcu test i i 12345
+setreg32.exe hkcu test s s sadglkjhgfhjgsakjg
+setreg32.exe hkcu test se se sadglkjhgfhjgsakjg
+setreg32.exe hkcu test t t 16:50:55
+setreg32.exe hkcu test dt dt "04.05.00 16:50:55"
+setreg32.exe hkcu test d d 04.05.00
+setreg32.exe hkcu test f f 12345.123
